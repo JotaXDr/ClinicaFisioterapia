59acdd142e6d3e64b44c98113cb6240aad70f27b
JotaXDr <109303186+JotaXDr@users.noreply.github.com>
2023-11-07 14:04:14 -0300

    Add files via upload

--- src/Components/Pag2/Pag2.jsx
+++ src/Components/Pag2/Pag2.jsx
@@ -3,7 +3,15 @@ function Pag2 (){
     return(
         <div className={styles.div}>
             <img className={styles.img} src="/image 2.png" alt=""/>
-            <p className={styles.par1}>"A fisioterapia desempenha um papel fundamental na promoção da saúde e recuperação de lesões. Como especialista nessa área, ressalto sua importância na melhoria da qualidade de vida e no bem-estar dos pacientes. Através de técnicas terapêuticas, exercícios e orientações personalizadas, os fisioterapeutas ajudam a restaurar a função física, aliviar a dor e prevenir futuras complicações. A fisioterapia é essencial tanto para atletas em busca de desempenho máximo quanto para pessoas com condições crônicas ou lesões agudas. Com um enfoque holístico e individualizado, os fisioterapeutas são essenciais para otimizar a saúde física e promover a recuperação completa."</p>
+            <p className={styles.par1}>A fisioterapia oferece uma variedade de serviços para promover a saúde e o bem-estar, incluindo:
+
+                Reabilitação pós-operatória
+                Tratamento de lesões esportivas
+                Gerenciamento da dor
+                Reabilitação neurológica
+                Fisioterapia respiratória
+                Prevenção de lesões
+                Esses serviços visam melhorar a mobilidade, aliviar a dor, fortalecer os músculos e melhorar a função geral do corpo. A fisioterapia é essencial para pacientes em diferentes estágios de recuperação, desde cirurgias até condições crônicas, ajudando-os a recuperar a independência funcional e melhorar sua qualidade de vida. Nossos especialistas oferecem todas essas especialidades e vão te tratar com o melhor zelo possivel.</p>
 
         </div>
     )
